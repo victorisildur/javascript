67e2bacabaa832d26d3bc569092686c3f78b758d
victorisildur <victorisildur@163.com>
2015-07-29 09:40:33 +0800

    convert title to meet jekyll format

--- convert-csdn/markdown-recieve.js
+++ convert-csdn/markdown-recieve.js
@@ -14,11 +14,19 @@ function mdRcv(idArr) {
             }).on('end', function() {
                 try {
                     var obj = JSON.parse(resChunkArr.join(''));
-                    var title = obj.data.title;
+                    var createTime = obj.data.create;
+                    createTime = createTime.substring(0, createTime.indexOf(' '));
+                    var title = createTime+'-'+obj.data.title;
                     console.log(title);
                     var markdownContent = obj.data.markdowncontent;
                     var file = fs.createWriteStream('./blogs/' + title + '.markdown');
-                    file.write(markdownContent);
+                    var header = '---\n';
+                    header += 'layout: post\n';
+                    header += 'title: ' + title + '\n';
+                    header += 'date: ' + obj.data.create + '\n';
+                    header += 'categories:' + obj.data.categories.substring(0, obj.data.categories.indexOf(',')) + '\n';
+                    header += '---\n';
+                    file.write(header + markdownContent);
                 } catch(e) {
                     console.error(url+'\n'+e);
                 }
